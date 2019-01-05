329c43334589d7815aeb9ca27cc434b8624b5f79
hx <599294384@qq.com>
2019-01-05 16:07:06 +0800

    一个小demo23

--- idea_git_60/src/main/java/BaseController.java
+++ idea_git_60/src/main/java/BaseController.java
@@ -2,15 +2,7 @@ public class BaseController {
     public static void main(String[] args) {
         System.out.println("O(∩_∩)O哈哈~");
         System.out.println("左侧");
-        int a = 0;
-        int b = 2;
-        add(a, b);
-        System.out.println("哈哈");
     }
 
-    public static void add(int a, int b) {
-        a += b;
-        System.out.println("好的呢，有" + a + "份你挑~~");
 
-    }
 }
